bbe665c87f56a1d242adb43b496403997183ab86
Shwipy <andresequeira99@gmail.com>
2025-11-08 22:20:47 +0000

    skibidi

---
+++ app/src/main/java/estgoh/andremariana/tam_proj/AddQuizz.kt
@@ -0,0 +1,20 @@
+package estgoh.andremariana.tam_proj
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddQuizz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_quizz)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
