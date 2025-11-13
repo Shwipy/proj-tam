cfc1ff4b2c9562b1dbec8643abc20fbd796b678c
Shwipy <andresequeira99@gmail.com>
2025-11-13 16:59:53 +0000

    Changed Text Color to White and Button Colors

--- app/src/main/java/estgoh/andremariana/tam_proj/QuizActivity.kt
+++ app/src/main/java/estgoh/andremariana/tam_proj/QuizActivity.kt
@@ -36,6 +36,7 @@ class QuizActivity : AppCompatActivity() {
         val btn_op_4 = findViewById<Button>(R.id.btn_option_4)
 
         val correct_color = ContextCompat.getColor(this, R.color.correct)
+        val new_text_color = ContextCompat.getColor(this, R.color.white)
 
         val right_answer_button = when(correct){
             1 -> btn_op_1
@@ -44,6 +45,7 @@ class QuizActivity : AppCompatActivity() {
             else -> btn_op_4
         }
         right_answer_button.backgroundTintList = ColorStateList.valueOf(correct_color)
+        right_answer_button.setTextColor(ColorStateList.valueOf(new_text_color))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,6 +73,7 @@ class QuizActivity : AppCompatActivity() {
 
         val correct_color = ContextCompat.getColor(this, R.color.correct)
         val wrong_color = ContextCompat.getColor(this, R.color.wrong)
+        val new_text_color = ContextCompat.getColor(this, R.color.white)
 
         if (answer_three == "null"){
             //https://www.geeksforgeeks.org/kotlin/how-to-make-a-button-invisible-in-android
@@ -90,8 +93,10 @@ class QuizActivity : AppCompatActivity() {
             if (correct_answer == 1){
 //              https://www.geeksforgeeks.org/android/how-to-change-the-background-color-of-button-in-android-using-colorstatelist
                 btn_op_1.backgroundTintList = ColorStateList.valueOf(correct_color)
+                btn_op_1.setTextColor(ColorStateList.valueOf(new_text_color))
             }else{
                 btn_op_1.backgroundTintList = ColorStateList.valueOf(wrong_color)
+                btn_op_1.setTextColor(ColorStateList.valueOf(new_text_color))
                 showCorrect(correct_answer)
             }
             disableButtons()
@@ -100,8 +105,10 @@ class QuizActivity : AppCompatActivity() {
         btn_op_2.setOnClickListener {
             if (correct_answer == 2){
                 btn_op_2.backgroundTintList = ColorStateList.valueOf(correct_color)
+                btn_op_2.setTextColor(ColorStateList.valueOf(new_text_color))
             }else{
                 btn_op_2.backgroundTintList = ColorStateList.valueOf(wrong_color)
+                btn_op_2.setTextColor(ColorStateList.valueOf(new_text_color))
                 showCorrect(correct_answer)
             }
             disableButtons()
@@ -111,8 +118,10 @@ class QuizActivity : AppCompatActivity() {
         btn_op_3.setOnClickListener {
             if (correct_answer == 3){
                 btn_op_3.backgroundTintList = ColorStateList.valueOf(correct_color)
+                btn_op_3.setTextColor(ColorStateList.valueOf(new_text_color))
             }else{
                 btn_op_3.backgroundTintList = ColorStateList.valueOf(wrong_color)
+                btn_op_3.setTextColor(ColorStateList.valueOf(new_text_color))
                 showCorrect(correct_answer)
             }
             disableButtons()
@@ -122,8 +131,10 @@ class QuizActivity : AppCompatActivity() {
         btn_op_4.setOnClickListener {
             if (correct_answer == 4){
                 btn_op_4.backgroundTintList = ColorStateList.valueOf(correct_color)
+                btn_op_4.setTextColor(ColorStateList.valueOf(new_text_color))
             }else{
                 btn_op_4.backgroundTintList = ColorStateList.valueOf(wrong_color)
+                btn_op_4.setTextColor(ColorStateList.valueOf(new_text_color))
                 showCorrect(correct_answer)
             }
             disableButtons()
